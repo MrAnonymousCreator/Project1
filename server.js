cc7e937ea92bba4d22dddc5892cd8d3a1a94d52b
Mranonymouscreator <aaron.kim110111@gmail.com>
2026-05-10 17:55:22 +1000

    Fixed PathError

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('/api/signals', (req, res) => {
 });
 
 // Serve React app for all other routes
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
